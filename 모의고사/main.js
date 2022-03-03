a2c9421836fe6e8d7d77158ecd29aaa9a77dcc20
SeunghoLEE <suhole7728@naver.com>
2022-03-03 19:53:09 +0900

    중복 순열 알고리즘 자료

--- 모의고사/main.js
+++ 모의고사/main.js
@@ -38,24 +38,6 @@ function solution(answers) {
 
 // 깔끔한 다른 풀이
 
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
 function solution(answers) {
     var answer = [];
     var a1 = [1, 2, 3, 4, 5];
@@ -74,3 +56,5 @@ function solution(answers) {
 
     return answer;
 }
+
+
