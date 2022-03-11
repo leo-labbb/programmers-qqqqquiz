4b305875b7f3211789288d21ab3d0e10c700b8b6
SeunghoLEE <suhole7728@naver.com>
2022-03-11 22:41:39 +0900

    문자열 내림차순으로 배치하기

---
+++ 문자열 내림차순으로 배치하기/main.js
@@ -0,0 +1,31 @@
+// 제 설명
+// 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+// s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+// 제한 사항
+// str은 길이 1 이상인 문자열입니다.
+// 입출력 예
+// s	return
+// "Zbcdefg"	"gfedcbZ"
+
+function solution(s) {
+    var answer = '';
+    let small = [], big = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i].toUpperCase()) big.push(s[i])
+        if (s[i] === s[i].toLowerCase()) small.push(s[i])
+    }
+    big.sort();
+    small.sort();
+    answer = small.reverse().join('') + big.reverse().join('')
+    return answer;
+}
+
+// 좀 더 깔끔한 풀이
+function solution(s) {
+    return s
+            .split('')
+            .sort()
+            .reverse()
+            .join('')
+}
