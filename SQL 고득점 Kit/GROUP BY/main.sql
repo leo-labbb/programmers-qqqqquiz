68cdaade3816a9e94ee32b26fc3b706e09aa0b16
SeunghoLEE <suhole7728@naver.com>
2022-03-14 15:30:45 +0900

    GROUP BY

---
+++ SQL 고득점 Kit/GROUP BY/main.sql
@@ -0,0 +1,48 @@
+-- 문제 설명
+-- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+
+-- NAME	TYPE	NULLABLE
+-- ANIMAL_ID	VARCHAR(N)	FALSE
+-- ANIMAL_TYPE	VARCHAR(N)	FALSE
+-- DATETIME	DATETIME	FALSE
+-- INTAKE_CONDITION	VARCHAR(N)	FALSE
+-- NAME	VARCHAR(N)	TRUE
+-- SEX_UPON_INTAKE	VARCHAR(N)	FALSE
+
+고양이와 개는 몇 마리 있을까
+
+-- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 이때 고양이를 개보다 먼저 조회해주세요.
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) FROM ANIMAL_INS GROUP BY ANIMAL_TYPE ORDER BY ANIMAL_TYPE
+
+동명 동물 수 찾기
+
+-- 동물 보호소에 들어온 동물 이름 중 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 SQL문을 작성해주세요. 이때 결과는 이름이 없는 동물은 집계에서 제외하며, 결과는 이름 순으로 조회해주세요.
+
+SELECT NAME, COUNT(NAME) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME is NOT NULL
+GROUP BY NAME
+HAVING COUNT(NAME) >= 2
+ORDER BY NAME
+
+입양 시각 구하기(1)
+
+-- 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 이때 결과는 시간대 순으로 정렬해야 합니다.
+
+SELECT HOUR(DATETIME) AS HOUR, COUNT(DATETIME) AS COUNT
+FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) >= 9 AND HOUR(DATETIME) <= 19
+GROUP BY HOUR(DATETIME)
+ORDER BY HOUR(DATETIME)
+
+입양 시각 구하기(2)
+
+-- 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 0시부터 23시까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 이때 결과는 시간대 순으로 정렬해야 합니다.
+
+SET @hour := -1;
+
+SELECT (@hour := @hour + 1) AS HOUR,
+(SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) AS COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23
