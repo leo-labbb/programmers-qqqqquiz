02d15765da837ec89e23bcdbb3a1313a7fbec8c7
SeunghoLEE <suhole7728@naver.com>
2022-03-25 21:38:14 +0900

    소수 찾기2

--- Level 2/소수 찾기/main.js
+++ Level 2/소수 찾기/main.js
@@ -41,3 +41,47 @@ function solution(numbers) {
         }
         return true;
     }
+
+// others
+function solution(numbers) {
+    let answer = 0;
+    let primeNumbers = [];
+
+    const numbersArr = numbers.split("");
+
+    // 소수 판별 함수
+    const determinePrime = (num) => {
+        let count = 0;
+        for (let i = 1; i <= num; i++) {
+            if (num % i === 0) {
+                count++;
+            }
+            if (count >= 3) {
+                break;
+            }
+        }
+        if (count === 2 && !primeNumbers.includes(num)) {
+            primeNumbers.push(num);
+        }
+    };
+
+    // 숫자 만들기
+    const mergeNumbers = (arr, str) => {
+        if (arr.length > 0) {
+            for (let i = 0; i < arr.length; i++) {
+                const temp = [...arr];
+                temp.splice(i, 1);
+                mergeNumbers(temp, str + arr[i]);
+            }
+        }
+
+        if (str.length > 0) {
+            determinePrime(+str);
+        }
+    };
+
+    mergeNumbers(numbersArr, "");
+
+    answer = primeNumbers.length;
+    return answer;
+}
